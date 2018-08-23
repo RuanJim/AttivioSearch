d0a388420409cc4ba862d3a6dac09b73f27a5760
Jim <jim.ruan@perkinelmer.com>
2018-08-23 15:44:40 +0800

    Revert "Update code."
    This reverts commit 33d9b4a519e6922b1ca5eee510b960ef2c0b46a5.

--- AttivioSearch/CustomAddIn.cs
+++ AttivioSearch/CustomAddIn.cs
@@ -1,32 +1,15 @@
-// --------------------------------------------------------------------------------------------------------------------
-// <copyright file="CustomAddIn.cs" company="PerkinElmer Inc.">
-//   Copyright (c) 2013 PerkinElmer Inc.,
-//     940 Winter Street, Waltham, MA 02451.
-//     All rights reserved.
-//     This software is the confidential and proprietary information
-//     of PerkinElmer Inc. ("Confidential Information"). You shall not
-//     disclose such Confidential Information and may not use it in any way,
-//     absent an express written license agreement between you and PerkinElmer Inc.
-//     that authorizes such use.
-// </copyright>
-// --------------------------------------------------------------------------------------------------------------------
-#region
+using System;
+using System.Windows.Forms;
 
+using Spotfire.Dxp.Application;
 using Spotfire.Dxp.Application.Extension;
 
-#endregion
-
-namespace Com.PerkinElmer.Service.AttivioSearch
+namespace AttivioSearch
 {
     /// <summary>
     /// </summary>
     public sealed class CustomAddIn : AddIn
     {
-        protected override void RegisterVisuals(VisualRegistrar registrar)
-        {
-            base.RegisterVisuals(registrar);
-
-            registrar.Register(new AttivioSearchFactory());
-        }
+        // Override methods in this class to register your extensions.
     }
 }
