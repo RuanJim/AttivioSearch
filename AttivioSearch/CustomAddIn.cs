06bff5709b6f53dbedb1f494f45a008a28213fe7
Jim <jim.ruan@perkinelmer.com>
2018-08-23 14:15:02 +0800

    Add project files.

---
+++ AttivioSearch/CustomAddIn.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Windows.Forms;
+
+using Spotfire.Dxp.Application;
+using Spotfire.Dxp.Application.Extension;
+
+namespace AttivioSearch
+{
+    /// <summary>
+    /// </summary>
+    public sealed class CustomAddIn : AddIn
+    {
+        // Override methods in this class to register your extensions.
+    }
+}
