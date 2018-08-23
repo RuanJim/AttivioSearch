fffe4ada49620965807a91710996631459eb4f16
Jim <jim.ruan@perkinelmer.com>
2018-08-23 15:55:01 +0800

    Update project settings.

--- AttivioSearch/CustomAddIn.cs
+++ AttivioSearch/CustomAddIn.cs
@@ -1,10 +1,23 @@
-using System;
-using System.Windows.Forms;
+// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="CustomAddIn.cs" company="PerkinElmer Inc.">
+//   Copyright (c) 2013 PerkinElmer Inc.,
+//     940 Winter Street, Waltham, MA 02451.
+//     All rights reserved.
+//     This software is the confidential and proprietary information
+//     of PerkinElmer Inc. ("Confidential Information"). You shall not
+//     disclose such Confidential Information and may not use it in any way,
+//     absent an express written license agreement between you and PerkinElmer Inc.
+//     that authorizes such use.
+// </copyright>
+// --------------------------------------------------------------------------------------------------------------------
 
-using Spotfire.Dxp.Application;
+#region
+
 using Spotfire.Dxp.Application.Extension;
 
-namespace AttivioSearch
+#endregion
+
+namespace Com.PerkinElmer.Service.AttivioSearch
 {
     /// <summary>
     /// </summary>
