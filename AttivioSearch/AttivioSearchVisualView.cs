bca794c46a217acf86c5584f18eed165f0dddcd3
Jim <jim.ruan@perkinelmer.com>
2018-08-23 16:19:18 +0800

    Add attivio search visual view.

---
+++ AttivioSearch/AttivioSearchVisualView.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Spotfire.Dxp.Application;
+using Spotfire.Dxp.Application.Extension;
+
+namespace Com.PerkinElmer.Service.AttivioSearch
+{
+    public sealed class AttivioSearchVisualView : CustomVisualView<AttivioSearchVisual>
+    {
+        public AttivioSearchVisualView(AttivioSearchVisual model) : base(model)
+        {
+        }
+
+        protected override HttpContent GetResourceCore(string path, NameValueCollection query, AttivioSearchVisual snapshotNode)
+        {
+            return base.GetResourceCore(path, query, snapshotNode);
+        }
+
+        protected override void ModifyCore(string method, string args, AttivioSearchVisual liveNode)
+        {
+            base.ModifyCore(method, args, liveNode);
+        }
+
+        protected override void OnUpdateRequiredCore()
+        {
+        }
+    }
+}
