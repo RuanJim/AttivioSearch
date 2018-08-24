f5dce88efd5fee661b866f5955a495d3bb606190
Jim <jim.ruan@perkinelmer.com>
2018-08-24 10:44:19 +0800

    Update code.

--- AttivioSearch/AttivioSearchVisualView.cs
+++ AttivioSearch/AttivioSearchVisualView.cs
@@ -22,22 +22,10 @@ namespace Com.PerkinElmer.Service.AttivioSearch
 
         protected override HttpContent GetResourceCore(string path, NameValueCollection query, AttivioSearchVisual snapshotNode)
         {
-            return new HttpContent("text/html", @"<!DOCTYPE html>
-<html style=""width: 100%; height: 100%; overflow: hidden;"">
-<head>
-    <script>
-        window.addEventListener(""message"", function (event) {
-            Spotfire.modify("""", event.data);
-        }, false);
-    </script>
-</head>
-<body style=""width: 100%; height: 100%; margin: 0px;"">
-<iframe id=""att"" src=""http://192.168.20.113:8080/searchui/"" style=""width: 100%; height: 100%; border: 0px solid black;"">
-    
-</iframe>
-</body>
-</html>
-");
+            var bytes = GetEmbeddedResource("Com.PerkinElmer.Service.AttivioSearch.web.Attivio.html");
+            var mimeType = ResolveContentType("Com.PerkinElmer.Service.AttivioSearch.web.Attivio.html");
+
+            return new HttpContent(mimeType, bytes);
         }
 
         protected override void ModifyCore(string method, string args, AttivioSearchVisual liveNode)
