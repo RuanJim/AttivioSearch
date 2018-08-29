6c38636d6c3517aba0406ae01722054270670b44
Jim <jim.ruan@perkinelmer.com>
2018-08-29 13:55:13 +0800

    Add attivio server url preference.

---
+++ AttivioSearch/AttivioSearchPreference.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Spotfire.Dxp.Application.Extension;
+using Spotfire.Dxp.Framework.Persistence;
+using Spotfire.Dxp.Framework.Preferences;
+
+namespace Com.PerkinElmer.Service.AttivioSearch
+{
+    [Serializable]
+    [PersistenceVersion(1, 0)]
+    public class AttivioSearchPreference : CustomPreference
+    {
+        private readonly PreferenceProperty<string> _attivioServerUrl;
+
+        public AttivioSearchPreference()
+        {
+            _attivioServerUrl = AddPreference(new PreferenceProperty<string>(
+                "AttivioServerUrl",
+                "1.0",
+                PreferencePersistenceScope.Server, 
+                PreferenceUsage.UserGroup
+                ));
+        }
+
+        public string AttivioServerUrl
+        {
+            get { return _attivioServerUrl.Value; }
+            set { _attivioServerUrl.Value = value; }
+        }
+
+        public override string Category => "Attivio";
+
+        public override string SubCategory => "SearchUI Server Url";
+    }
+}
